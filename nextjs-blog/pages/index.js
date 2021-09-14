c4126a6d273a50766a314ea41083fe11fcc1c4c4
4ren <81793795+ryrepositories@users.noreply.github.com>
2021-09-14 03:03:39 +0000

    Initial commit

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello there,  Welcome to my first site. This was an interesting journey. I did not enjoy as much as I wish that I would but as I learn more I am sure I will enjoy it much better. Knowledge is power!!</p>
+      
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
